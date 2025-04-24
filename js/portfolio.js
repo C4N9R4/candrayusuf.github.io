5cb018bee92f8ed9d1e630f2468a92d2efeda441
ProgrammingBasicTrainer <chandrayusuf365@gmail.com>
2025-04-24 10:40:22 +0700

    Update file index.htm dan portfolio.js

---
+++ js/portfolio.js
@@ -0,0 +1,55 @@
+AOS.init({
+  once: true,
+});
+//////////////////////////
+document.addEventListener("DOMContentLoaded", function(){
+const form = document.querySelector('form');
+const btnKirim = document.querySelector('btnKirim');
+const btnLoading = document.querySelector('.btn-Loading');
+const alertSucces = document.getElementById("thanks");
+
+form.addEventListener("submit", function(e){
+    fetch(form.action,{
+      method: 'POST'
+      body: new FormData(form),
+
+    })
+      .then(response => {
+        btnLoading.classList.add("d-none");
+        btnKirim.classList.remove('d-none');
+        alertSucces.classList.remove('d-none');
+        form.reset();
+      })
+      .catch(error => console.error('Error:', error));
+});
+});
+form.addEventListener("submit", (e) => {
+  btnLoading.classList.remove("d-none");
+  btnKirim.classList.add("d-none");
+})
+/////////////////////////////
+gsap.from(".jumbotron img ", {
+  y: -200,
+  duration: 1.5,
+  ease: "bounce.out",
+  opacity: 0,
+});
+gsap.from(".navbar", {
+  duration: 2,
+  ease: "back-out(1.7)",
+  y: -300,
+  opacity: 0,
+});
+gsap.from(".Myheading", {
+  duration: 1,
+  rotateY: 660,
+  opacity: 0,
+});
+/////////////////////////////
+
+//////Vanila javascript//////////
+VanillaTilt.init(document.querySelectorAll(".tools-box"), {
+  max: 30,
+  speed: 300,
+  glare: true,
+});
